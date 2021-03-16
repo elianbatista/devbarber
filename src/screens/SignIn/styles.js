efca87bf1e644ec309c5f5e7b373460339f66bc1
Elian Batista <elianbatista2000@gmail.com>
2021-03-15 22:38:34 -0300

    signin

--- src/screens/SignIn/styles.js
+++ src/screens/SignIn/styles.js
@@ -5,7 +5,7 @@ export const Container = styled.SafeAreaView`
   background-color: #63C2D1;
   flex: 1;
   justify-content: center;
-  align-items: center;
+  align-items: center; 
 `
   
 export const InputArea = styled.View`
