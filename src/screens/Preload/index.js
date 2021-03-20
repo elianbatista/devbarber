b9b1f3378488cc8f2ce545d99f3c9fc2d8aa69c7
Elian Batista <elianbatista2000@gmail.com>
2021-03-20 17:43:19 -0300

    preload screen

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,18 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useContext } from 'react'
 import { Container, LoadingIcon } from './styles'
 import { AsyncStorage } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
-
+import { UserContext } from '../../contexts/UserContext'
 import BarberLogo from '../../assets/barber.svg'
 
 const Preload = () => {
+  const { dispatch: userDispatch } = useContext(UserContext)
   const navigation = useNavigation()
 
   useEffect(() => {
     const checkToken = async () => {
       const token = await AsyncStorage.getItem('token')
       if (token) {
-        
+        let res = await Api.checkToken(token)
+        if (res.token) {
+          await AsyncStorage.setItem('token', res.token)
+          userDispatch({
+            type: 'setAvatar',
+            payload: {
+              avatar: res.data.avatar
+            }
+          })
+          navigation.reset({
+            routes: [{
+              name: 'MainTab'
+            }]
+          })
+        } else {
+          navigation.navigate('SignIn')
+        }
       } else {
         navigation.navigate('SignIn')
       }
