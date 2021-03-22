bb78dd90aa76cd2610f00674288527bf1718bb35
Elian Batista <elianbatista2000@gmail.com>
2021-03-22 17:12:59 -0300

    custom tab bar

--- src/components/CustomTabBar.js
+++ src/components/CustomTabBar.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components/native'
+
+import { UserContext } from '../contexts/UserContext'
 
 import HomeIcon from '../assets/home.svg'
 import SearchIcon from '../assets/search.svg'
@@ -19,11 +21,51 @@ const TabItem = styled.TouchableOpacity`
   align-items: center;
 `
 
-const CustomTabBar = () => {
+const TabItemCenter = styled.TouchableOpacity`
+  width: 70px;
+  height: 70px;
+  justify-content: center;
+  align-items: center;
+  background-color: #FFF;
+  border-radius: 35px;
+  border: 3px solid #4EADBE;
+  margin-top: -20px;
+`
+
+const AvatarIcon = styled.Image`
+  width: 24px;
+  height: 24px;
+  border-radius: 12px;
+`
+
+const CustomTabBar = ({ state, navigation }) => {
+  const { state: user } = useContext(UserContext)
+
+  const goTo = (screenName) => {
+    navigation.navigate(screenName)
+  }
+
   return (
     <TabArea>
-      <TabItem>
-        <HomeIcon width="24" height="24" fill="#FFFFFF" />
+      <TabItem onPress={() => goTo('Home')}>
+        <HomeIcon style={{opacity: state.index === 0 ? 1 : 0.5}} width="24" height="24" fill="#FFFFFF" />
+      </TabItem>
+      <TabItem onPress={() => goTo('Search')}>
+        <SearchIcon style={{opacity: state.index === 1 ? 1 : 0.5}} width="24" height="24" fill="#FFFFFF" />
+      </TabItem>
+      <TabItemCenter onPress={() => goTo('Appointments')}>
+        <TodayIcon width="32" height="32" fill="#4EADBE" />
+      </TabItemCenter>
+      <TabItem onPress={() => goTo('Favorites')}>
+        <FavoriteIcon style={{opacity: state.index === 3 ? 1 : 0.5}} width="24" height="24" fill="#FFFFFF" />
+      </TabItem>
+      <TabItem onPress={() => goTo('Profile')}>
+        { 
+          user.avatar != '' ?
+            <AvatarIcon source={{uri: user.avatar}} /> 
+            :
+            <AccountIcon style={{opacity: state.index === 4 ? 1 : 0.5}} width="24" height="24" fill="#FFFFFF" />
+        }
       </TabItem>
     </TabArea>
   )
