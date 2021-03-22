b0470ad556ca3ab19d052e24901b92586862c0ca
Elian Batista <elianbatista2000@gmail.com>
2021-03-22 17:13:11 -0300

    home styles

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -4,3 +4,31 @@ import styled from 'styled-components/native'
 export const Container = styled.SafeAreaView`
   
 `
+
+export const Scroller = styled.ScrollView`
+
+`
+
+export const HeaderArea = styled.View`
+
+`
+
+export const HeaderTitle = styled.Text`
+
+`
+
+export const SearchButton = styled.TouchableOpacity`
+
+`
+
+export const LocationArea = styled.View`
+
+`
+
+export const LocationInput = styled.TextInput`
+
+`
+
+export const LocationFinder = styled.TouchableOpacity`
+  
+`
