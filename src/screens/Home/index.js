12b5ad49787341ac67c1f833b0fc1be056ac25ac
Elian Batista <elianbatista2000@gmail.com>
2021-03-23 21:34:28 -0300

    home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { RefreshControl } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import Api from '../../Api'
+import * as Location from 'expo-location'
 import { 
   Container,
   Scroller,
@@ -7,30 +11,109 @@ import {
   SearchButton,
   LocationArea,
   LocationInput,
-  LocationFinder
+  LocationFinder,
+  LoadingIcon,
+  ListArea
 } from './styles'
-
+import BarberItem from '../../components/BarberItem'
 import SearchIcon from '../../assets/search.svg'
 import MyLocationIcon from '../../assets/my_location.svg'
 
 export default () => {
+  const navigation = useNavigation()
+
+  const [locationText, setLocationText] = useState('')
+  const [coords, setCoords] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [list, setList] = useState([])
+  const [refreshing, setRefreshing] = useState(false)
+
+  const handleLocationFinder = async () => {
+    setCoords(null)
+    let { status } = await Location.requestPermissionsAsync()
+    if (status == 'granted') {
+      setLoading(true)
+      setLocationText('')
+      setList([])
+      let location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High })
+      setCoords(location.coords)
+      getBarbers()
+    }
+  }
+
+  const getBarbers = async () => {
+    setLoading(true)
+    setList([])
+
+    let lat = null
+    let lng = null
+    if (coords) {
+      lat = coords.latitude
+      lng = coords.longitude
+    } 
+
+    let res = await Api.getBarbers(lat, lng, locationText)
+    if (res.error == '') {  
+      if (res.loc) {
+        setLocationText(res.loc)
+      }
+      setList(res.data)
+    } else {
+      alert('Erro: ' + res.error)
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getBarbers()
+  }, [])
+
+  const onRefresh = () => {
+    setRefreshing(false)
+    getBarbers()
+  }
+
+  const handleLocationSearch = () => {
+    setCoords({})
+    getBarbers()
+  }
+
   return (
     <Container>
-      <Scroller>
+      <Scroller refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }>
         <HeaderArea>
           <HeaderTitle numberOfLines={2}>
             Encontre o seu barbeiro favorito
           </HeaderTitle>
-          <SearchButton>
+          <SearchButton onPress={() => navigation.navigate('Search')}>
             <SearchIcon width="26" height="26" fill="#FFFFFF" />
           </SearchButton>
         </HeaderArea>
         <LocationArea>
-          <LocationInput />
-          <LocationFinder>
+          <LocationInput 
+            placeholder="Onde você está?" 
+            placeholderTextColor="#FFFFFF" 
+            value={locationText}
+            onChangeText={text => setLocationText(text)}
+            onEndEditing={handleLocationSearch}
+          />
+          <LocationFinder onPress={handleLocationFinder}>
             <MyLocationIcon width="24" height="24" fill="#FFFFFF" />
           </LocationFinder>
         </LocationArea>
+        {
+          loading &&
+          <LoadingIcon size="large" color="#FFFFFF" />
+        }
+        <ListArea>
+          {
+            list.map((item, key) => (
+              <BarberItem key={key} data={item} />
+            ))
+          }
+        </ListArea>
       </Scroller>
     </Container>
   )
